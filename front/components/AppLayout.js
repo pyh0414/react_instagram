a20a5bd38650d57bd98b4394178b7d1d0a2c3f44
yeonho Park <whwlsvy12@naver.com>
2019-06-11 00:36:18 +0900

    add signup modal

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -9,24 +9,31 @@ const AppLayoutWrapper = styled.div`
   height: 100vh;
   width: 100vw;
 `;
+
 const AppLayout = ({ children }) => {
+  const dummy = {
+    me: { id: "pyh" },
+    isLoggedIn: false
+  };
   return (
-    <>
-      <Header />
-      <AppLayoutWrapper>
-        <Row>
-          <Col xs={2} md={6}>
-            6
-          </Col>
-          <Col xs={4} md={12}>
-            12
-          </Col>
-          <Col xs={2} md={6}>
-            6
-          </Col>
-        </Row>
-      </AppLayoutWrapper>
-    </>
+    <div>
+      <>
+        <Header />
+        <AppLayoutWrapper>
+          <Row>
+            <Col xs={2} md={6}>
+              6
+            </Col>
+            <Col xs={4} md={12}>
+              <children />
+            </Col>
+            <Col xs={2} md={6}>
+              6
+            </Col>
+          </Row>
+        </AppLayoutWrapper>
+      </>
+    </div>
   );
 };
 
