354d9ec4b4e8434d1658fd30102b4ff6e6e946c7
yeonho Park <whwlsvy12@naver.com>
2019-06-18 00:44:37 +0900

    add index.js

---
+++ back/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const expressSession = require("express-session");
+const dotenv = require("dotenv");
+
+// const userAPIRouter = require("./routes/user");
+
+const db = require("./models");
+db.sequelize.sync();
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true, // 자바스크립트에서 쿠키게 접근 못함
+      secure: false // https쓸때 true
+    },
+    name: "pyh"
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(3000, () => {
+  console.log("server is running on 3000");
+});
