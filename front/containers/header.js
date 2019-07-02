598d87c8e5335b8ac3b9b574f8b3dc18ae299742
yeonho Park <whwlsvy12@naver.com>
2019-07-03 02:09:25 +0900

    separate signup logic

--- front/containers/header.js
+++ front/containers/header.js
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Input, Form, Icon, Button } from "antd";
-import Modal from "react-modal";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
-
-import { SIGN_UP_REQUEST } from "../reducer/user";
 
 const HeaderWrapper = styled.div`
   height: 70px;
@@ -41,54 +37,6 @@ const Search = styled.div`
 const Header = () => {
   const dummy = {
     user: { id: "pyh" }
-  };
-
-  const [modalStatus, setModalStatus] = useState(false);
-  const [id, setChangeId] = useState("");
-  const [name, setChangeName] = useState("");
-  const [password, setChangePassword] = useState("");
-  const [passwordCheck, setChangePasswordCheck] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const onChangeId = e => {
-    setChangeId(e.target.value);
-  };
-
-  const onChangeName = e => {
-    setChangeName(e.target.value);
-  };
-
-  const onChangePassword = e => {
-    setChangePassword(e.target.value);
-  };
-  const onChangePasswordCheck = e => {
-    setPasswordError(e.target.value !== password);
-    setChangePasswordCheck(e.target.value);
-  };
-
-  const openModal = () => {
-    setModalStatus(true);
-  };
-
-  const closeModal = () => {
-    setModalStatus(false);
-  };
-
-  const onSubmitForm = e => {
-    e.preventDefault();
-    if (password !== passwordCheck) {
-      return setPasswordError(true);
-    }
-    dispatch({
-      type: SIGN_UP_REQUEST,
-      data: {
-        id,
-        password,
-        name
-      }
-    });
   };
 
   return (
@@ -139,100 +87,6 @@ const Header = () => {
               <Button type="primary" onClick={openModal}>
                 회원가입
               </Button>
-              {modalStatus && (
-                <Modal
-                  isOpen={modalStatus}
-                  onRequestClose={closeModal}
-                  style={customStyles}
-                  contentLabel="Example Modal"
-                >
-                  <Form onSubmit={onSubmitForm}>
-                    <div>
-                      <label htmlFor="user-id">아이디</label>
-                      <br />
-
-                      <Input
-                        name="user-id"
-                        value={id}
-                        onChange={onChangeId}
-                        required
-                        prefix={
-                          <Icon
-                            type="user"
-                            style={{ color: "rgba(0,0,0,.25)" }}
-                          />
-                        }
-                      />
-                      <br />
-                      <br />
-                    </div>
-                    <div>
-                      <label htmlFor="user-id">이름</label>
-                      <br />
-
-                      <Input
-                        name="user-id"
-                        value={name}
-                        onChange={onChangeName}
-                        required
-                      />
-                      <br />
-                      <br />
-                    </div>
-                    <div>
-                      <label htmlFor="user-password">비밀번호</label>
-                      <br />
-
-                      <Input
-                        name="user-password"
-                        value={password}
-                        onChange={onChangePassword}
-                        required
-                        prefix={
-                          <Icon
-                            type="lock"
-                            style={{ color: "rgba(0,0,0,.25)" }}
-                          />
-                        }
-                        type="password"
-                      />
-                      <br />
-                      <br />
-                    </div>
-                    <div>
-                      <label htmlFor="user-password">비밀번호확인</label>
-                      <br />
-                      <Input
-                        name="user-password-check"
-                        value={passwordCheck}
-                        onChange={onChangePasswordCheck}
-                        required
-                        prefix={
-                          <Icon
-                            type="lock"
-                            style={{ color: "rgba(0,0,0,.25)" }}
-                          />
-                        }
-                        type="password"
-                      />
-                    </div>
-                    <br />
-                    {passwordError && (
-                      <div style={{ color: "red" }}>
-                        비밀번호가 일치하지 않습니다.
-                      </div>
-                    )}
-
-                    <Button type="primary" htmlType="submit">
-                      가입
-                    </Button>
-
-                    <Button type="primary" onClick={closeModal}>
-                      닫기
-                    </Button>
-                  </Form>
-                </Modal>
-              )}
             </Form.Item>
           </Form>
         )}
