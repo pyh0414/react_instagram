400be316caf71cabc80b57946779c5bb4697ad94
yeonho Park <whwlsvy12@naver.com>
2019-07-04 23:52:57 +0900

    home setting

--- front/pages/home.js
+++ front/pages/home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
-import PostForm from "../containers/PortForm";
+import Article from "../containers/Article";
+import Header from "../containers/Header";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #fafafa;
+  height: 100%;
+`;
 
 const Home = () => {
-  const dumy = {};
-  return <PostForm />;
+  return (
+    <div style={{ backgroundColor: "whilte" }}>
+      <Header />
+      <Wrapper>
+        <Article />
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Home;
