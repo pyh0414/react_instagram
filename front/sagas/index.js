24e4d684dee2c646d3f25867bc869ecab4733be1
yeonho Park <whwlsvy12@naver.com>
2019-07-21 03:39:03 +0900

    add user,post saga

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,9 +1,11 @@
 import { all, fork } from "redux-saga/effects";
-import user from "./user";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3060";
 
-export default function* rootSage() {
-  yield all([fork(user)]);
+import user from "./user";
+import post from "./post";
+
+export default function* rootSaga() {
+  yield all([fork(user), fork(post)]);
 }
