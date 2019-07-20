4055e5ec41cc759d0f25ec3ad3ff748cad0639ed
yeonho Park <whwlsvy12@naver.com>
2019-07-21 03:37:57 +0900

    add action

--- front/reducer/post.js
+++ front/reducer/post.js
@@ -11,14 +11,14 @@ export const UPLOAD_POST_IMAGE_REQUEST = "UPLOAD_POST_REQUEST";
 export const UPLOAD_POST_IMAGE_SUCCESS = "UPLOAD_POST_SUCCESS";
 export const UPLOAD_POST_IMAGE_FAILURE = "UPLOAD_POST_FAILURE";
 
-export const CLEAR_POST_IMAGEPATH_REQUEST =
-  "CLEAR_CLEAR_POST_IMAGEPATH_REQUEST";
-
-export const DELETE_POST_IMAGE_REQUEST = "DELETE_POST_IMAGE_REQUEST";
-
 export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
 export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
 export const ADD_POST_FAILURE = "ADD_POST_FAILURE";
+
+export const CLEAR_POST_IMAGEPATH_REQUEST =
+  "CLEAR_CLEAR_POST_IMAGEPATH_REQUEST";
+export const DELETE_POST_IMAGE_REQUEST = "DELETE_POST_IMAGE_REQUEST";
+export const CLEAR_POST_FORM_STATUS = "CLEAR_POST_FORM_STATUS";
 
 export default (state = initialState, action) => {
   return produce(state, draft => {
@@ -61,6 +61,11 @@ export default (state = initialState, action) => {
       case ADD_POST_SUCCESS: {
         draft.addPostResult = false;
         draft.isAddingPost = false;
+        break;
+      }
+
+      case CLEAR_POST_FORM_STATUS: {
+        draft.addPostResult = false;
         break;
       }
     }
