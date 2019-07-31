d2d9d69ba9dc7832504038dc91cfbbf8b74a229b
yeonho Park <whwlsvy12@naver.com>
2019-07-31 14:34:00 +0900

    add comment

---
+++ front/containers/PostCard/Body/Comment.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const ImgCustom = styled.img`
+  width: 28px;
+  height: 30px;
+  border-radius: 100%;
+`;
+
+const Comments = ({ comments }) => {
+  const reply = comments.map((v, i) => {
+    return (
+      <div style={{ marginTop: "3px" }}>
+        <span>
+          <ImgCustom src={`http://localhost:3060/${v.User.profile}`} />
+        </span>
+        <span
+          style={{ fontWeight: "bold", color: "#202020", marginLeft: "5px" }}
+        >
+          {v.User.userId}
+        </span>
+        <span style={{ marginLeft: "4px" }}>{v.content}</span>
+      </div>
+    );
+  });
+
+  return reply;
+};
+
+export default Comments;
