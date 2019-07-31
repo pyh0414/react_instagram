45aa17d23d9a5c3b88977583d586d54300557ded
yeonho Park <whwlsvy12@naver.com>
2019-07-31 18:23:57 +0900

    chat scroll fixed

--- front/containers/Home/Chat/index.js
+++ front/containers/Home/Chat/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
 
 const Chat = () => {
+  let roomSocket;
+  let chatSocket;
+
+  useEffect(() => {
+    roomSocket = io("localhost:3060/room");
+    chatSocket = io("localhost:3060/chat");
+  }, []);
   return <div>Chat</div>;
 };
 
