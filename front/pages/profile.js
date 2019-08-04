e2e42ac947a38e0ed075ca77e429115d35158207
yeonho Park <whwlsvy12@naver.com>
2019-08-04 17:46:11 +0900

    modify css

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,6 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
-import Header from "../containers/Header";
+import Header from "../containers/Home/Header";
+import myProfile from "../containers/profile/profile";
+import Posts from "../containers/profile/profile";
+
+const Wrapper = styled.div``;
+
 const Profile = () => {
   return (
     <>
