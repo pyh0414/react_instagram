dc6d352509be4c584df3ea30c939009634548b2d
yeonho Park <whwlsvy12@naver.com>
2019-08-08 16:16:45 +0900

    chats null뜨는 부분 수정

--- front/reducer/chat.js
+++ front/reducer/chat.js
@@ -72,7 +72,7 @@ export default (state = initialState, action) => {
       }
 
       case ENTER_ROOM_SUCCESS: {
-        const { id, Chats } = action.data.room;
+        const { id, Chats } = action.data;
         draft.currentRoom = { roomId: id };
         draft.currentRoom.chats = Chats;
         break;
@@ -84,7 +84,9 @@ export default (state = initialState, action) => {
       }
 
       case SEND_MESSAGE_SUCCESS: {
-        draft.currentRoom.chats.push(action.data);
+        if (draft.currentRoom) {
+          draft.currentRoom.chats.push(action.data);
+        }
         break;
       }
     }
