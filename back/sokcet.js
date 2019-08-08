7b1972bf1a30a6d67131a0159c2c973d0191f67e
yeonho Park <whwlsvy12@naver.com>
2019-08-08 16:11:56 +0900

    세션 미들웨어 제거, 이벤트 인자 수정

--- back/sokcet.js
+++ back/sokcet.js
@@ -1,25 +1,8 @@
 const socketIo = require("socket.io");
-const axios = require("axios");
-const passportSocketIo = require("passport.socketio");
 const session = require("express-session");
-const mysqlStore = require("connect-mysql")(session);
 
-module.exports = (server, app, sessionMiddleware) => {
+module.exports = (server, app) => {
   const io = socketIo(server);
-
-  // const sessionMiddleware = session({
-  //   resave: false,
-  //   saveUninitialized: false,
-  //   secret: "process.env.COOKIE_SECRET",
-  //   cookie: {
-  //     httpOnly: true, // 자바스크립트에서 쿠키게 접근 못함
-  //     secure: false // https쓸때 true
-  //   }
-  // });
-
-  io.use((socket, next) => {
-    sessionMiddleware(socket.request, socket.request.res, next);
-  });
 
   app.set("io", io);
   const room = io.of("/room");
@@ -32,14 +15,12 @@ module.exports = (server, app, sessionMiddleware) => {
     });
 
     socket.on("enter_room_request", room => {
-      console.log("enter_room_request", socket.id);
-      socket.join(room.data.room.id);
+      socket.join(room.id);
       socket.emit("enter_room_success", room);
     });
 
-    socket.on("out_room_request", roomId => {
-      console.log("out_room_request", socket.id);
-      socket.leave(roomId);
+    socket.on("out_room_request", room => {
+      socket.leave(room.id);
       socket.emit("out_room_success");
     });
   });
