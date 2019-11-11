2a05548f037c5d1a9d4e58c9cb6238658b93f076
yeonho Park <whwlsvy12@naver.com>
2019-11-11 20:08:03 +0900

    코드 포매팅

--- front/containers/Home/PostCard/Images/index.js
+++ front/containers/Home/PostCard/Images/index.js
@@ -2,21 +2,22 @@ import React, { useCallback } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  height: 500px;
-  width: 100%;
+	height: 500px;
+	width: 100%;
+	text-align: center;
 `;
 
 const ImgCustom = styled.img`
-  width: 100%;
-  height: 440px;
+	width: 100%;
+	height: auto;
 `;
 
 const Image = ({ images }) => {
-  return (
-    <Wrapper>
-      <ImgCustom src={`http://localhost:3060/${images[0].src}`} />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<ImgCustom src={`http://localhost:3060/${images[0].src}`} />
+		</Wrapper>
+	);
 };
 
 export default Image;
