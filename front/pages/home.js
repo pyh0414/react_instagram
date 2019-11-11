dd96234e5f79bb69a492115930013b524d2ab646
yeonho Park <whwlsvy12@naver.com>
2019-11-11 20:04:14 +0900

    코드 포매팅

--- front/pages/home.js
+++ front/pages/home.js
@@ -13,60 +13,60 @@ import { LOAD_MAIN_POSTS_REQUEST } from "../reducer/post";
 import { LOAD_CHAT_ROOM_REQUEST } from "../reducer/chat";
 
 const Wrapper = styled.div`
-  background-color: #fafafa;
-  height: 100%;
+	background-color: #fafafa;
+	height: 100%;
 `;
 
 const Side = styled.div`
-  width: 50%;
-  height: 60%;
-  margin-top: 40px;
-  margin-left: 40px;
+	width: 50%;
+	height: 60%;
+	margin-top: 40px;
+	margin-left: 40px;
 `;
 
 const Home = () => {
-  const { user } = useSelector(state => state.user);
-  const { mainPosts } = useSelector(state => state.post);
+	const { user } = useSelector((state) => state.user);
+	const { mainPosts } = useSelector((state) => state.post);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MAIN_POSTS_REQUEST
-    });
-    dispatch({
-      type: LOAD_CHAT_ROOM_REQUEST
-    });
-  }, []);
+	useEffect(() => {
+		dispatch({
+			type: LOAD_MAIN_POSTS_REQUEST
+		});
+		dispatch({
+			type: LOAD_CHAT_ROOM_REQUEST
+		});
+	}, []);
 
-  return (
-    <div style={{ backgroundColor: "whilte" }}>
-      <Header />
-      <Wrapper>
-        <Row>
-          <Col md={16}>
-            {mainPosts &&
-              mainPosts.map(v => {
-                return <PostCard key={v.id} post={v} />;
-              })}
-          </Col>
+	return (
+		<div style={{ backgroundColor: "whilte" }}>
+			<Header />
+			<Wrapper>
+				<Row>
+					<Col md={16}>
+						{mainPosts &&
+							mainPosts.map((v) => {
+								return <PostCard key={v.id} post={v} />;
+							})}
+					</Col>
 
-          <Col
-            md={8}
-            style={{
-              position: "sticky",
-              top: 0
-            }}
-          >
-            <Side>
-              <UserInfo user={user} />
-              <Messenger />
-            </Side>
-          </Col>
-        </Row>
-      </Wrapper>
-    </div>
-  );
+					<Col
+						md={8}
+						style={{
+							position: "sticky",
+							top: 0
+						}}
+					>
+						<Side>
+							<UserInfo user={user} />
+							<Messenger />
+						</Side>
+					</Col>
+				</Row>
+			</Wrapper>
+		</div>
+	);
 };
 
 export default Home;
