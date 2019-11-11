ecf530107a1dc26e4332eed9ab54e414c7ad7a0c
yeonho Park <whwlsvy12@naver.com>
2019-11-11 20:26:20 +0900

    이미지 크기 지정하지 않고, 원래 크기로 함

--- front/containers/Home/PostCard/Images/index.js
+++ front/containers/Home/PostCard/Images/index.js
@@ -1,22 +1,16 @@
-import React, { useCallback } from "react";
+import React from "react";
 import styled from "styled-components";
-
-const Wrapper = styled.div`
-	height: 500px;
-	width: 100%;
-	text-align: center;
-`;
 
 const ImgCustom = styled.img`
 	width: 100%;
-	height: auto;
+	height: 100%;
 `;
 
 const Image = ({ images }) => {
 	return (
-		<Wrapper>
+		<>
 			<ImgCustom src={`http://localhost:3060/${images[0].src}`} />
-		</Wrapper>
+		</>
 	);
 };
 
