a0fe0d1fb5a6ab74cb6b6828a4496c8d22828ece
yeonho Park <whwlsvy12@naver.com>
2019-11-11 20:02:58 +0900

    로그인 성공/정보 재확인 로직 수정

--- front/pages/index.js
+++ front/pages/index.js
@@ -8,105 +8,105 @@ import { useDispatch, useSelector } from "react-redux";
 import { LOG_IN_REQUEST } from "../reducer/user";
 
 const Wrapper = styled.div`
-  height: 70vh;
-  display: flex;
+	height: 70vh;
+	display: flex;
 `;
 
 const CustomForm = styled(Form)`
-  margin: auto;
-  width: 22%;
+	margin: auto;
+	width: 22%;
 `;
 
 import { CONNECT_SOCKET_REQUEST } from "../reducer/chat";
 
 const SignUp = ({ children }) => {
-  const [id, setChangeId] = useState("");
-  const [password, setChangePassword] = useState("");
+	const [id, setChangeId] = useState("");
+	const [password, setChangePassword] = useState("");
 
-  const dispatch = useDispatch();
-  const { hasLoginRequestFinished, user } = useSelector(state => state.user);
+	const dispatch = useDispatch();
+	const { hasLoginRequestFinished, user } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    if (hasLoginRequestFinished) {
-      dispatch({
-        type: CONNECT_SOCKET_REQUEST
-      });
-      message.success("로그인 되었습니다");
-      Router.push("/home");
-      if (user) {
-      } else {
-        message.success("아이디 또는 비밀번호를 확인해 주세요");
-      }
-    }
-  }, [user, hasLoginRequestFinished]);
+	useEffect(() => {
+		if (hasLoginRequestFinished) {
+			if (user) {
+				dispatch({
+					type: CONNECT_SOCKET_REQUEST
+				});
+				message.success("로그인 되었습니다");
+				Router.push("/home");
+			} else {
+				message.success("아이디 또는 비밀번호를 확인해 주세요");
+			}
+		}
+	}, [user, hasLoginRequestFinished]);
 
-  const onChangeId = e => {
-    setChangeId(e.target.value);
-  };
+	const onChangeId = (e) => {
+		setChangeId(e.target.value);
+	};
 
-  const onChangePassword = e => {
-    setChangePassword(e.target.value);
-  };
+	const onChangePassword = (e) => {
+		setChangePassword(e.target.value);
+	};
 
-  const onSubmitForm = e => {
-    e.preventDefault();
-    dispatch({
-      type: LOG_IN_REQUEST,
-      data: {
-        id,
-        password
-      }
-    });
-    setChangeId("");
-    setChangePassword("");
-  };
+	const onSubmitForm = (e) => {
+		e.preventDefault();
+		dispatch({
+			type: LOG_IN_REQUEST,
+			data: {
+				id,
+				password
+			}
+		});
+		setChangeId("");
+		setChangePassword("");
+	};
 
-  return (
-    <Wrapper>
-      {children}
-      <CustomForm onSubmit={onSubmitForm}>
-        <div>
-          <br />
+	return (
+		<Wrapper>
+			{children}
+			<CustomForm onSubmit={onSubmitForm}>
+				<div>
+					<br />
 
-          <Input
-            name="user-id"
-            placeholder="이메일"
-            value={id}
-            onChange={onChangeId}
-            required
-            prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
-          />
-          <br />
-        </div>
-        <div>
-          <br />
-          <Input
-            name="user-password"
-            placeholder="비밀번호"
-            value={password}
-            onChange={onChangePassword}
-            required
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-          />
-          <br />
-        </div>
-        <br />
-        <Button
-          type="primary"
-          htmlType="submit"
-          style={{ width: "40%", marginRight: "20%" }}
-        >
-          로그인
-        </Button>
-        <Button type="Default" style={{ width: "40%" }}>
-          <Link href="/signUp">
-            <a> 회원가입</a>
-          </Link>
-        </Button>
-      </CustomForm>
-    </Wrapper>
-  );
+					<Input
+						name="user-id"
+						placeholder="이메일"
+						value={id}
+						onChange={onChangeId}
+						required
+						prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
+					/>
+					<br />
+				</div>
+				<div>
+					<br />
+					<Input
+						name="user-password"
+						placeholder="비밀번호"
+						value={password}
+						onChange={onChangePassword}
+						required
+						prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+						type="password"
+					/>
+					<br />
+				</div>
+				<br />
+				<Button
+					type="primary"
+					htmlType="submit"
+					style={{ width: "40%", marginRight: "20%" }}
+				>
+					로그인
+				</Button>
+				<Button type="Default" style={{ width: "40%" }}>
+					<Link href="/signUp">
+						<a> 회원가입</a>
+					</Link>
+				</Button>
+			</CustomForm>
+		</Wrapper>
+	);
 };
 
 export default SignUp;
