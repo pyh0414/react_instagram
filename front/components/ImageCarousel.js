9b1452b322b51a33c152fed23b56d1fdbc5febcf
yeonho Park <whwlsvy12@naver.com>
2019-11-11 20:08:50 +0900

    carousel 이미지가 상위 컴포넌트 벗어나는거 수정, 이미지 가운데 정렬

--- front/components/ImageCarousel.js
+++ front/components/ImageCarousel.js
@@ -2,66 +2,69 @@ import React from "react";
 import Slider from "react-slick";
 import Head from "next/head";
 
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
 
 const ImageCarousel = ({ images }) => {
-  const setting = {
-    dots: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true
-  };
+	const setting = {
+		dots: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true
+	};
 
-  return (
-    <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </Head>
-      {images.length > 1 ? (
-        <div
-          style={{
-            paddingRight: "30px",
-            paddingLeft: "30px",
-            backgroundColor: "black",
-            textAlign: "center"
-          }}
-        >
-          <Slider {...setting}>
-            {images.map((v, i) => {
-              return (
-                <div>
-                  <img
-                    src={`http://localhost:3060/${v.src}`}
-                    style={{
-                      height: "420px",
-                      width: "100%"
-                    }}
-                  />
-                </div>
-              );
-            })}
-          </Slider>
-        </div>
-      ) : (
-        <img
-          src={`http://localhost:3060/${images[0].src}`}
-          style={{ height: "100%", width: "100%" }}
-        />
-      )}
-    </div>
-  );
+	const Wrapper = styled.div`
+		width: 400px;
+		height: 415px;
+		text-align: center;
+	`;
+
+	const ImageWrapper = styled.img`
+		width: auto;
+		height: 415px;
+		margin: 0px auto;
+	`;
+	return (
+		<div>
+			<Head>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					charset="UTF-8"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+				/>
+			</Head>
+			{images.length > 1 ? (
+				<div
+					style={{
+						paddingRight: "30px",
+						paddingLeft: "30px",
+						backgroundColor: "black",
+						textAlign: "center"
+					}}
+				>
+					<Slider {...setting}>
+						{images.map((v, i) => {
+							return (
+								<Wrapper key={i}>
+									<ImageWrapper src={`http://localhost:3060/${v.src}`} />
+								</Wrapper>
+							);
+						})}
+					</Slider>
+				</div>
+			) : (
+				<Wrapper>
+					<ImageWrapper src={`http://localhost:3060/${images[0].src}`} />
+				</Wrapper>
+			)}
+		</div>
+	);
 };
 
 export default ImageCarousel;
